58b6c311796b62ce7385fc275c8c68dc92029fbe
Oryam <48219965+or-yam@users.noreply.github.com>
2020-07-04 21:10:02 +0300

    bug fixed

--- dist/main.js
+++ dist/main.js
@@ -1,42 +1,44 @@
 // BAD PRACTICE - not proper MVC. Should be separated to files.
 const render = function (todos) {
+  $('#todos').empty();
 
-    $("#todos").empty()
-
-    todos.forEach(todo => {
-        $("#todos").append(`
-        <div data-id=${todo._id} class="todo ${todo.complete ? 'complete' : ''}">
+  todos.forEach((todo) => {
+    $('#todos').append(`
+        <div data-id=${todo.id} class="todo ${
+      todo.completed ? 'complete' : ''
+    }">
             <i class="fas fa-check-circle"></i>
-            <span class=text>todo.text</span>
+            <span class=text>${todo.text}</span>
             <span class="delete"><i class="fas fa-trash"></i></span>
         </div>
-        `)
-    })
-}
+        `);
+  });
+};
 
+//add post
 const add = function () {
-    $.post('/todo', { text: $("#todo-input").val() }, function (todos) {
-        render(todos)
-        $("#todo-input").val("")
-    })
-}
+  $.post('/todo', { text: $('#todo-input').val() }, function (todos) {
+    render(todos);
+    $('#todo-input').val('');
+  });
+};
+//mark done
+$('#todos').on('click', '.fa-check-circle', function () {
+  const id = $(this).closest('.todo').data().id;
+  $.ajax({
+    method: 'PUT',
+    url: '/todo/' + id,
+    success: (todos) => render(todos),
+  });
+});
+//delete
+$('#todos').on('click', '.fa-trash', function () {
+  const id = $(this).closest('.todo').data().id;
+  $.ajax({
+    method: 'DELETE',
+    url: '/todo/' + id,
+    success: (todos) => render(todos),
+  });
+});
 
-$("#todos").on("click", ".fa-check-circle", function () {
-    const id = $(this).closest(".todo").data().id
-    $.ajax({
-        method: "PUT",
-        url: "/todo/" + id,
-        success: todos => render(todos)
-    })
-})
-
-$("#todos").on("click", ".fa-trash", function () {
-    const id = $(this).closest(".todo").data().id
-    $.ajax({
-        method: "DELETE",
-        url: "/todo/" + id,
-        success: todos => render(todos)
-    })
-})
-
-$.get('/todos', todos => render(todos))
+$.get('/todos', (todos) => render(todos));
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,33 +1,41 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const todos = []
-let id = 1
+let todos = [];
+let id = 1;
 
 router.get('/todos', function (req, res) {
-    res.send(todos)
-})
+  res.send(todos);
+});
 
 router.post('/todo', function (req, res) {
-    const text = req.body.text
-    const newTodo = { id: id++, text: text, complete: false }
+  const text = req.body.text;
+  const newTodo = { id: id++, text: text, complete: false };
 
-    todos.push(newTodo)
-    res.send(todos)
-})
+  todos.push(newTodo);
+  res.send(todos);
+});
 
 router.put('/todo/:todoID', function (req, res) {
-    const todoID = req.params.todoID
+  const todoID = req.params.todoID;
+  let status = todos.find((t) => t.id == todoID).completed;
+  if (status) {
+    todos.find((t) => t.id == todoID).completed = false;
+  } else {
+    todos.find((t) => t.id == todoID).completed = true;
+  }
 
-    todos.find(t => t.id == todoID).completed = true
-    res.send(todos)
-})
+  res.send(todos);
+});
 
 router.delete('/todo/:todoID', function (req, res) {
-    const todoID = req.params.todoID
-    todos.splice(todoID, 1)
-    console.log('hello')
-    res.send(todos)
-})
+  const todoID = req.params.todoID;
+  for (const task of todos) {
+    if (task.id == todoID) {
+      todos.splice(todos.indexOf(task), 1);
+    }
+  }
+  res.send(todos);
+});
 
-module.exports = router
+module.exports = router;
